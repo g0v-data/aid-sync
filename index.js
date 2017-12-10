5ee398130f8a8943c64981c06cb5ee773b946e24
Yuren Ju (yurenju) <yurenju@gmail.com>
2017-12-10 23:43:00 +0800

    Fixed for upstream website update

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ require('dotenv').config();
 var fs = require('fs');
 var jsdom = require('jsdom');
 var async = require('async');
+var path = require('path');
+var os = require('os');
 var anticaptcha = require('anti-captcha');
 var service = anticaptcha('http://anti-captcha.com', process.env.ANTI_CAPTCHA_KEY);
 
 var userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko';
 var username = env.RVIS_USERNAME;
 var password = env.RVIS_PASSWORD;
-var baseUrl = 'http://rvis-manage.mohw.gov.tw';
+var baseUrl = 'https://rvis-manage.mohw.gov.tw';
 var loginUrl = baseUrl + '/login.do';
 var downloadUrl = baseUrl + '/jsp/sg/sg2/sg25020_rpt.jsp';
 var captchaUrl = baseUrl + '/ImageServlet';
 var htmlFilename = 'out/aid.html';
 var csvFilename = 'out/aid.csv';
 var RE_COOKIE = /Set-Cookie: (.+?)=(.+?);/;
+var captchaFile = path.join(os.tmpdir(), 'captcha.jpg')
 
 module.exports = function(finish) {
   mkdir('-p', 'out');
@@ -27,21 +30,32 @@ module.exports = function(finish) {
       console.log('opening first page');
       jsdom.env({
         url: loginUrl,
+        strictSSL: false,
         cookieJar,
         done: function(err, window) {
-          done(null, window.document.cookie);
+          done(err);
         }
       });
     },
-    function(cookie, done) {
-      console.log('download captcha');
-      var downloadCaptchaCommand = `curl '${captchaUrl}' -H 'User-Agent: ${userAgent}' -H 'Cookie: ${cookie}' -o /tmp/captcha.jpg`;
+    function(done) {
+      const cookie = cookieJar.store.idx['rvis-manage.mohw.gov.tw']['/']['JSESSIONID'].value;
+      console.log('download captcha, cookie: ' + cookie);
+      const command = [
+        'curl',
+        captchaUrl,
+        `-H 'User-Agent: ${userAgent}'`,
+        `-H 'Cookie: JSESSIONID=${cookie}'`,
+        `-o ${captchaFile}`,
+        '--insecure'
+      ];
+      var downloadCaptchaCommand = command.join(' ');
       exec(downloadCaptchaCommand);
+      console.log(downloadCaptchaCommand);
       done(null, cookie);
     },
     function(cookie, done) {
       console.log('decode...');
-      var captcha = fs.readFileSync('/tmp/captcha.jpg');
+      var captcha = fs.readFileSync(captchaFile);
       var base64 = new Buffer(captcha).toString('base64');
       service.uploadCaptcha(base64, {phrase: true})
       .then(captcha => service.getText(captcha))
@@ -55,9 +69,10 @@ module.exports = function(finish) {
       var loginCommand = [
         'curl',
         '--user-agent "' + userAgent + '" ',
-        `-H 'Cookie: ${cookie}'`,
+        `-H 'Cookie: JSESSIONID=${cookie}'`,
         `--data "yn_cert=N&user_id=${process.env.ENCODED_RVIS_USERNAME}&user_pwd=${process.env.ENCODED_RVIS_PASSWORD}&checkcode=${captcha}"`,
         '--connect-timeout 5 --max-time 5 --retry 15',
+        '--insecure',
         '-i',
         loginUrl
       ].join(' ');
@@ -68,16 +83,21 @@ module.exports = function(finish) {
 
     function(cookie, done) {
       echo('downloading html');
+      const headers = cat('headers.txt').trim().split('\n').map(h => `-H "${h.trim()}"`);
+      const json = JSON.parse(cat('params.json'));
+      const params = Object.keys(json).map(k => `${k}=${json[k]}`);
       var downloadCommand = [
         'curl',
-        '-H \'Content-Type: application/x-www-form-urlencoded\'',
-        '-H "Cookie: ' + cookie + '"',
-        '--data \'prog_id=SG25020&qcityno=6300000000\'',
-        '--connect-timeout 5 --max-time 30 --retry 15 -v',
+        headers.join(' '),
+        `-H 'Cookie: JSESSIONID=${cookie}'`,
+        `--data "${params.join('&')}"`,
+        '--connect-timeout 60 --max-time 60 --retry 15',
+        '--insecure',
         '-o ' + htmlFilename,
         downloadUrl
       ].join(' ');
 
+      console.log('download command: ', downloadCommand);
       exec(downloadCommand);
       var html = cat(htmlFilename);
       done(null, html);
@@ -85,10 +105,11 @@ module.exports = function(finish) {
 
     function(html, done) {
       console.log('parsing csv');
-      jsdom.env(
+      jsdom.env({
         html,
-        ['http://code.jquery.com/jquery.js'],
-        function (err, window) {
+        features: { ProcessExternalResources : ['script']},
+        scripts: ['http://code.jquery.com/jquery.js'],
+        done: function(error, window) {
           echo('parsing to csv file');
           var $ = window.$;
 
@@ -107,7 +128,7 @@ module.exports = function(finish) {
           rm(htmlFilename);
           done(null);
         }
-      );
+      });
     }
   ], finish);
 }
